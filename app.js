d8ad7292f00ab464972ad0c06af906e748fe399e
Babajide777 <j.oyafemi@gmail.com>
2020-07-12 10:28:17 -0400

    app.js updated

--- app.js
+++ app.js
@@ -48,6 +48,6 @@ app.use(function(err, req, res, next) {
 module.exports = app;
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("server running on port 3000");
 });
